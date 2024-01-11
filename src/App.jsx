c8ddf1a213e42fa3b9d40dfdae06fc37e329b09d
Omar Lo Giudice <146293874+FearXz@users.noreply.github.com>
2024-01-11 17:57:00 +0100

    new project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./assets/css/style.min.css";
+import "./App.css";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route></Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
